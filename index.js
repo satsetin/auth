429cb0ca500f5db20c7e5b585c6220435d7375cf
sipatir08 <fathir080604@gmail.com>
2024-12-06 10:39:16 +0700

    push

---
+++ index.js
@@ -0,0 +1,20 @@
+import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
+
+if (getCookie("login")===""){
+    redirect("/");
+}
+
+getJSON("https://asia-southeast2-awangga.cloudfunctions.net/satsetin/data/user","login",getCookie("login"),responseFunction)
+
+function responseFunction(result){
+    if (result.status === 404){
+        setInner("content","Silahkan lakukan pendaftaran terlebih dahulu "+result.data.name);
+        redirect("/signup");
+    }else{
+        setInner("content","Selamat datang "+result.data.name);
+    }
+    console.log(result);
+}
